03ec07e7b262a955b7e985accabef7fc8919527c
ShahSaminYasar <shahsaminyasarraihan@gmail.com>
2026-03-30 19:29:21 +0600

    Added: Stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Stats from "./components/Stats";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <main>
         <Hero />
+        <Stats />
       </main>
       <Footer />
     </>
---
+++ src/components/Stats.jsx
@@ -0,0 +1,27 @@
+const Stats = () => {
+  return (
+    <section className="bg-gradient text-white">
+      <div className="container font-light flex flex-col sm:flex-row justify-evenly items-center">
+        <div>
+          <span className="text-6xl font-bold block mb-2">50K+</span>
+          <span className="text-white/90">Active Users</span>
+        </div>
+
+        <div className="divider sm:divider-horizontal"></div>
+
+        <div>
+          <span className="text-6xl font-bold block mb-2">200+</span>
+          <span className="text-white/90">Premium Tools</span>
+        </div>
+
+        <div className="divider sm:divider-horizontal"></div>
+
+        <div>
+          <span className="text-6xl font-bold block mb-2">4.9</span>
+          <span className="text-white/90">Rating</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Stats;
